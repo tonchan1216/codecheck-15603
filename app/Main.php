845a922f81ce38f24d9a7b13fb0adec7f368f736
tonchan1216 <viva.la.vida27315@gmail.com>
2017-08-26 20:46:04 +0900

    mb convert

--- app/Main.php
+++ app/Main.php
@@ -6,7 +6,6 @@ function run ($argc, $argv){
 		$jsonUrl = "memo.json"; //JSONファイルの場所とファイル名を記述
 		if(file_exists($jsonUrl)){
 		  $json = file_get_contents($jsonUrl);
-		  $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
 		  global $memo;
 		  $memo = json_decode($json,true);
 		}else {
